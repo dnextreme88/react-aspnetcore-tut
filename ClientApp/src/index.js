c6d3fca01fa90e2736326ad1f0900fa929d66a04
dnextreme88 <storm_iz_here@yahoo.com>
2021-08-06 09:09:36 +0800

    Final steps

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -8,17 +8,34 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store';
 
 import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from './auth0-wrapper';
+import config from './auth_config.json';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 const store = configureStore({});
 
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl ? 
+      appState.targetUrl : window.location.pathname
+  );
+}
+
 ReactDOM.render(
   <Provider store={store}>
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback} >
     <BrowserRouter basename={baseUrl}>
       <App />
     </BrowserRouter>
+  </Auth0Provider>
   </Provider>,
   rootElement);
 
